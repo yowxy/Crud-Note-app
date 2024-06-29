df73e56f769dcf35879e9c9a4d3280fb3b634860
yowxy <iklilblender@gmail.com>
2024-06-29 18:16:11 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import './index.css'
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id:'1',
+      textt:'Helloo',
+    }
+  ]);
+  const [inputNote, setInputNote] = useState("")
+  function handleInputChange(e){
+   setInputNote(e.target.value);
+  }
+
+  function handleInputSubmit (e){
+
+    e.preventDefault()
+
+    if(inputNote.trim() !== ""){
+      const newNote = {
+        id: new Date().getTime(),
+        textt: inputNote
+      }
+
+      setNotes([...notes, newNote]);
+      setInputNote("")
+    }
+  }
+
+
+  function handleDeleteNote (id){
+      const updateNotes = notes.filter((note) => note.id !== id);
+      setNotes(updateNotes);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Notes</h1>
+      <form className='note-input' onSubmit={handleInputSubmit}>
+        <input type='text' placeholder='Add a some text' value={inputNote}  onChange={handleInputChange}/>
+        <button >Add button</button>
+      </form>
+      <ul className='note-list'>
+          {notes.map((note) => (
+            <li key={note.id}>
+            {note.textt}
+            <button onClick={ ()=>  handleDeleteNote(note.id)}>Delete</button>
+            
+            </li>
+         
+          ))}
+      </ul>
     </div>
   );
 }
